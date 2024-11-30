f1978707430f31727504fb673b07d0e5c039fbc4
alamindev <mdalamin6554@gmail.com>
2024-11-30 20:19:29 +0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { ghPages } from 'vite-plugin-gh-pages';
-
-// Replace `your-github-username` and `repository-name` accordingly
+ 
 export default defineConfig({
   base: '/repository-name/',
-  plugins: [react(), ghPages()],
+  plugins: [react()],
 });
